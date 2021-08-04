66d75ff71277d989225f772576e3d64f1f1fd0cd
AayushBangroo <43703855+AayushBangroo@users.noreply.github.com>
2021-08-04 15:47:44 +0530

    first commit

---
+++ Backend/authorization/src/main/java/com/authorizationservice/authorization/AuthorizationApplication.java
@@ -0,0 +1,33 @@
+package com.authorizationservice.authorization;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.authorizationservice.authorization.model.AuthenticationRequest;
+import com.authorizationservice.authorization.repository.AuthRequestRepo;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@SpringBootApplication
+public class AuthorizationApplication implements CommandLineRunner{
+	
+	@Autowired
+	AuthRequestRepo authRequestRepo;
+	
+	public static void main(String[] args) {
+		SpringApplication.run(AuthorizationApplication.class, args);
+	}
+	
+	@Override
+	public void run(String... args) throws Exception{
+		AuthenticationRequest user1 = new AuthenticationRequest("aayush","aayush01");
+		AuthenticationRequest user2 = new AuthenticationRequest("ansh","ansh01");
+		
+		authRequestRepo.save(user1);
+		authRequestRepo.save(user2);
+	}
+
+}
